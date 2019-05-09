101231178f131ef49513329e03d1875b596bd263
Juan Felipe Ramos Correa <jf.ramos@uniandes.edu.co>
2019-05-09 15:27:32 -0500

    Add files

--- visualizations/app/fichas/fichas.js
+++ visualizations/app/fichas/fichas.js
@@ -1,3 +1,16 @@
 function cargarAutores(){
-    
+    let iframe= document.getElementById("frameAct");
+    iframe.setAttribute("src","autor.html");
+}
+function cargarEditoriales(){
+    let iframe= document.getElementById("frameAct");
+    iframe.setAttribute("src","editorial.html");
+}
+function cargarLibros(){
+    let iframe= document.getElementById("frameAct");
+    iframe.setAttribute("src","libro.html");
+}
+function cargarIlustradores(){
+    let iframe= document.getElementById("frameAct");
+    iframe.setAttribute("src","ilustrador.html");
 }
